c572131ca2010104da8b426786ee8e22864760b7
tsunose <tsunose.developer@gmail.com>
2022-11-24 22:55:08 +0900

    modify: convert file path logic

--- src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
+++ src/client/foundation/components/media/TrimmedImage/TrimmedImage.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 
-const convertFilePath = (src) => {
+const convertFilePath = (src, width) => {
   const paths = src.split('/')
   // (e.g.) 100_001.webp
-  const converted = `100_${paths.pop().replace('jpg', 'webp')}`
+  const converted = `${width}_${paths.pop().replace('jpg', 'webp')}`
   paths.push(converted);
   return paths.join('/');
 }
@@ -17,5 +17,5 @@ const convertFilePath = (src) => {
 
 /** @type {React.VFC<Props>} */
 export const TrimmedImage = ({ height, src, width }) => {
-  return <img src={convertFilePath(src)} height={height} width={width} loading="lazy" />;
+  return <img src={convertFilePath(src, width)} height={height} width={width} loading="lazy" />;
 };
