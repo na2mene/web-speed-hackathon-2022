8cd76857d74695c3281317f378beefb93e2cf9d5
tsunose <tsunose.developer@gmail.com>
2022-11-27 03:51:51 +0900

    mod

--- src/server/typeorm/initialize.js
+++ src/server/typeorm/initialize.js
@@ -1,7 +1,12 @@
 import fs from "fs/promises";
+import sqlite3 from 'sqlite3';
 
 import { DATABASE_PATH, INITIAL_DATABASE_PATH } from "../paths.js";
 
 export async function initialize() {
   await fs.copyFile(INITIAL_DATABASE_PATH, DATABASE_PATH);
+  const db = new sqlite3.Database(DATABASE_PATH);
+  db.run('create index testindex on betting_ticket(raceId, userId);');
+  db.run('create index test2index on odds_item(raceId);');
+  db.close();
 }
